db2f8c6c3f606f1667fe5bcab52751fef2d94c84
Anastasia Huseva <plotva@Anastasias-MacBook-Air.local>
2023-04-18 22:46:11 +0200

    project updated

--- app.js
+++ app.js
@@ -2,17 +2,14 @@
 const date = (document.getElementById('date').innerHTML =
   new Date().getFullYear());
 
-const yurek = date;
-const nasya = 'masyasya';
-// // setup nav
-// const navBtn = document.getElementById('nav-btn');
-// const navbar = document.getElementById('navbar');
-// const navClose = document.getElementById('nav-close');
-// // show nav
-// navBtn.addEventListener('click', () => {
-//   navbar.classList.add('showNav');
-// });
-// // close nav
-// navClose.addEventListener('click', () => {
-//   navbar.classList.remove('showNav');
-// });
+// navbar toggle
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav');
+});
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav');
+});
